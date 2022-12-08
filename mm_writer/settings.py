0f015c036ab4686634f82e4f265a2c254e9e327f
martinezmarcos93 <115021940+martinezmarcos93@users.noreply.github.com>
2022-12-08 01:22:56 -0300

    "migrate"

--- mm_writer/settings.py
+++ mm_writer/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'perfil_webb.apps.PerfilWebConfig',
+    'perfil_web.apps.PerfilWebConfig',
     
 ]
 
