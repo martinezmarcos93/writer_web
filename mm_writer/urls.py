0fa3972a7608dcbb7bf479e9d0fb6abc2400f27a
martinezmarcos93 <115021940+martinezmarcos93@users.noreply.github.com>
2022-12-08 01:26:42 -0300

    "primer intento falló"

--- mm_writer/urls.py
+++ mm_writer/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from perfil_web.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('perfil_web/', index, name='home'),
 ]
